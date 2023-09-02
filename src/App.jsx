c87168f4aeba079402334972cc26e6c7722c9aaa
darren <darrenxychen@gmail.com>
2023-09-01 22:20:49 -0700

    added card array

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
 import './App.css';
 
+const cards = [
+  { id: 1, name: 'Card 1' },
+  { id: 2, name: 'Card 2' },
+  { id: 3, name: 'Card 3' },
+  { id: 4, name: 'Card 4' },
+  { id: 5, name: 'Card 5' },
+  { id: 6, name: 'Card 6' },
+]
+
 function App() {
   return (
     <div className="App">
