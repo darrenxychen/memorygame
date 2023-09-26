1939018d9ef71e82e811375535942b4ee4710859
darren <darrenxychen@gmail.com>
2023-09-25 18:07:52 -0700

    added rewrites for vercel & abt page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Leaderboard from './pages/Leaderboard';
 import Navigation from './components/Navigation';
+import About from './pages/About';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/leaderboard" element={<Leaderboard />} />
+          <Route path="/about" element={<About />} />
           <Route path="*" element={<h1>Not Found</h1>} /> {/* 404 page */}
         </Routes>
       </div>
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+function About() {
+  return (
+    <div className="about">
+      <h1>About Us!</h1>
+      <p>It's just me, my name is Darren.
+        Thanks for playing!
+      </p>
+    </div>
+  );
+}
+
+export default About;
