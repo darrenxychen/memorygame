a42b9d9c8fe9e6c6aa096a9f3da88fca7dd367da
darren <darrenxychen@gmail.com>
2023-10-30 23:32:26 -0700

    added starter for the leaderboard

--- src/pages/Leaderboard.jsx
+++ src/pages/Leaderboard.jsx
@@ -1,10 +1,36 @@
 import './Leaderboard.css'
+import React, { useState, useEffect } from 'react';
 
 function Leaderboard() {
+  const [leaderboardData, setLeaderboardData] = useState([
+    { id: 1, name: 'Alice', time: '02:15', turns: 10 },
+    { id: 2, name: 'Bob', time: '01:30', turns: 8 },
+    { id: 3, name: 'Charlie', time: '03:00', turns: 12 },
+    // ... more data
+  ]);
   return (
     <div className="leaderboard">
       <h1>Leaderboard</h1>
-      <p>Coming soon...</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Rank</th>
+            <th>Name</th>
+            <th>Time</th>
+            <th>Turns</th>
+          </tr>
+        </thead>
+        <tbody>
+          {leaderboardData.map((player, index) => (
+            <tr key={player.id}>
+              <td>{index + 1}</td>
+              <td>{player.name}</td>
+              <td>{player.time}</td>
+              <td>{player.turns}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
