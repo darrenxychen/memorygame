a182c59eeea82d44337c153e98d96f08df912682
dabren4 <darrenxychen@gmail.com>
2024-05-19 19:45:29 -0700

    added firebase for leaderboard

---
+++ src/firebase/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7Axox5bUt7QuIoMqbecESHMQzMsbYMBg",
+  authDomain: "memorygame-6b9d2.firebaseapp.com",
+  projectId: "memorygame-6b9d2",
+  storageBucket: "memorygame-6b9d2.appspot.com",
+  messagingSenderId: "888533924388",
+  appId: "1:888533924388:web:f72c0d6975854324b976b1"
+};
+
+const app = initializeApp(firebaseConfig);
